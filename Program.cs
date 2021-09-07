ecf8d1e7164912efb60daa231c9bb2fd8654e8f9
KononenkoDaria <daria.kononenko.a@gmail.com>
2021-09-07 17:35:11 +0300

    Lab1

--- Program.cs
+++ Program.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Лабораторна_робота__1
+namespace Lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
 
+            double g = 9.8;
+            double time;
+            double height;
+            string answer;
+
+            Console.WriteLine("Обчислення часу вільного падіння тіла...");
+            Console.WriteLine("Введіть висоту (в метрах): ");
+
+            answer = Console.ReadLine();
+
+            height = Convert.ToDouble(answer);
+
+            time = Math.Sqrt(2 * height / g);
+
+            Console.WriteLine($"Час вільного падіння тіла {time} с.");
         }
     }
 }
